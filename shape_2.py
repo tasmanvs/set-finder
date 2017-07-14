2ef55e5eea0d6de713a5578230f75501a1409259
tasmanvs <tasmanvs@gmail.com>
2017-07-14 12:37:09 -0700

    Added set card detection
    Needs to be trimmed down, variables should be renamed, more comments needed. In general needs to be cleaned up.

---
+++ shape_2.py
@@ -0,0 +1,237 @@
+import cv2
+import matplotlib.pyplot as plt
+import imutils
+import numpy as np
+
+
+#ok I don't even need this. Each coord could just be stored in a numpy array
+class coord(object):
+	"""docstring for coord"""
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+	# def __init__(self, coords):
+	# 	self.x = coords[0]
+	# 	self.y = coords[1]
+
+	#operator overloading:
+	def __str__(self):
+		return("(" + str(self.x) + ", " + str(self.y) + ")")
+	def __repr__(self):
+   		return str(self)
+	def __eq__(self, other):
+		return(self.x == other.x and self.y == other.y)
+
+
+	# member functions
+	def findSlope(self, other):
+		dy = self.y - other.y
+		dx = self.x - other.x
+
+		if (dx == 0):
+			slope  = 999999999999999999999
+		else:
+			slope = float(dy)/dx
+
+		deriv = coord(slope, self.x)
+		return deriv
+
+	def asArray(self): #eugh...
+		return [self.x, self.y]
+
+# should change this to create a seperate image for each card
+def createContours(imageFile):
+	# import image
+	image = cv2.imread(imageFile)
+
+	# preprocess the image
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	# blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+	thresh = cv2.threshold(gray, 220, 255, cv2.THRESH_BINARY)[1]
+
+	# find contours in the thresholded image
+	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
+	cnts = cnts[0] if imutils.is_cv2() else cnts[1] #version check?
+
+
+	# loop over the contours
+	for c in cnts:
+		# compute the center of the contour
+		# print "contour is", c
+		M = cv2.moments(c)
+		cX = 0
+		cY = 0
+		if (M["m00"] != 0):
+			cX = int(M["m10"] / M["m00"])
+			cY = int(M["m01"] / M["m00"])
+
+	boundingRects = [cv2.boundingRect(c) for c in cnts] #returns x,y,width,height (x,y) is top left
+	boundingRects_points = []
+	masks = [image[b[1]:b[1]+b[3], b[0]:b[0]+b[2]] for b in boundingRects]
+	
+	return image, cnts
+
+#contour is a numpy array
+# only good where images are aligned with frame. Could fix by finding 4 max in second derivative of the contour
+def findContourCorners(contour, image):
+	topright = -1000000
+	tr = coord(0,0)
+	topleft = -1000000
+	tl = coord(0,0)
+	bottomleft = -1000000
+	bl = coord(0,0)
+	bottomright = -1000000
+	br = coord(0,0)
+
+	for point in contour:
+		print "point is", point
+		x,y = point[0][0],point[0][1]
+		print "    ",x,y
+		if (x + y >= topright):
+			tr = coord(x,y)
+			topright = x + y
+		if (x - y >= bottomright):
+			br = coord(x,y)
+			bottomright = x - y
+		if (-1*x + y >= topleft):
+			tl = coord(x,y)
+			topleft = y - x
+		if (-1*x - y >= bottomleft):
+			bl = coord(x,y)
+			bottomleft = -1*x - y
+
+	cv2.drawContours(image, [contour], -1, (0,255,0), 2)
+
+
+	circleRadius = 5
+	cv2.circle(image, (tr.x, tr.y), circleRadius, (255, 0, 0), -1)
+	cv2.circle(image, (tl.x, tl.y), circleRadius, (255, 255, 0), -1)
+	cv2.circle(image, (br.x, br.y), circleRadius, (255, 0, 255), -1)
+	cv2.circle(image, (bl.x, bl.y), circleRadius, (0, 255, 255), -1)
+
+	return [[tl.x, tl.y], [tr.x, tr.y], [bl.x, bl.y], [br.x, br.y]] #return the coordinates in [] form
+
+def findContourCorners2(contour, image): #done with second derivative
+
+	conts = []
+	d1_conts = [] # y' , x ===> y'' = y'_1 - y'2 / x_1 - x_2
+	d2_conts = [] # y'' , x
+	for point in contour:
+		x,y = point[0][0], point[0][1]
+		c = coord(x,y)
+		conts.append(c)
+
+
+	pointShift = 2 # how many points to we want to skip before calculating the slope?
+
+	for idx, point in enumerate(conts): #find slope of current point and the one after the next one.
+		nextPoint = (idx+pointShift) % len(conts) # loop around if we go over
+		slope = conts[idx].findSlope(conts[nextPoint])
+		d1_conts.append(slope)
+
+	print d1_conts
+	for idx, d1 in enumerate(d1_conts):
+		nextPoint = (idx+pointShift) % len(d1_conts) # loop around if we go over
+		slope = d1_conts[idx].findSlope(d1_conts[nextPoint])
+		d2_conts.append(slope)
+
+	for s in d2_conts:
+		print "slope slope is", s
+
+	#find the four highest magnitude values of d2
+
+	highest_d2 = [0,0,0,0]
+
+	highest_coords = [coord(0,0) for i in range(4)]
+
+	d2_conts_y_val = [val.x for val in d2_conts]
+
+	for idx, d2 in enumerate(d2_conts_y_val):
+		t = 0
+		biggerFound = False
+		while (t < len(highest_d2) and abs(d2) > abs(highest_d2[t])):
+			t += 1
+			biggerFound = True
+		if (biggerFound == True):
+			highest_d2[t-1] = d2
+			highest_coords[t-1] = conts[idx]
+
+	print highest_d2
+	print highest_coords
+
+	circleRadius = 5
+	cv2.circle(image, (highest_coords[0].x, highest_coords[0].y), circleRadius, (255, 0, 0), -1)
+	cv2.circle(image, (highest_coords[1].x, highest_coords[1].y), circleRadius, (255, 255, 0), -1)
+	cv2.circle(image, (highest_coords[2].x, highest_coords[2].y), circleRadius, (255, 0, 255), -1)
+	cv2.circle(image, (highest_coords[3].x, highest_coords[3].y), circleRadius, (0, 255, 255), -1)
+
+#change the image using affine transform such that the contour fits the new image.
+#should turn the perspective warped cards into rectangles.
+def fixPerspective(contour, image):
+
+	# get image dimensions
+	height, width, channels = image.shape
+	
+	# set source and dest_rect
+	source = findContourCorners(contour, image)
+
+	# dest_rect = tl, tr, bl, br
+
+	dest_rect = [[0,0], [width, 0], [0,height], [width, height]] #may have to make some negative
+
+
+	pts1 = np.float32(source)
+	pts2 = np.float32(dest_rect)
+
+	print "pts1", pts1
+	print "pts2", pts2
+
+	M = cv2.getPerspectiveTransform(pts1,pts2)
+	dst = cv2.warpPerspective(img,M,(width,height))
+
+	return dst
+
+
+
+
+#----------->>>>>>>>>>>>>--------------_>>>>>>>>>>>>>>>>>...------------__>>>>>>>>>>>>>>>
+
+
+img, contours = createContours("set.jpg")
+
+# for rect in boundingRects:
+# 	[x,y,w,h] = [val for val in rect]
+# 	tl = [x,y]
+# 	tr = [x+w, y]
+# 	bl = [x, y-h]
+# 	br = [x+w, y-h]
+# 	point_list = [tl, tr, bl, br]
+# 	boundingRects_points.append(point_list)
+
+plt.subplot(321),plt.imshow(img),plt.axis('off')
+
+dst = []
+for c in contours:
+	dst.append(fixPerspective(c, img))
+
+
+# assuming in rows of 3
+# probably either 4 or 5 cols.
+
+num_rows = len(contours) / 3 + 1 #(one more for source)
+
+
+plt.subplot(num_rows, 3, 1),plt.imshow(img),plt.axis('off') #display the source at top left
+
+for idx, d in enumerate(dst):
+	plt.subplot(num_rows, 3, num_rows*3 - idx),plt.imshow(d),plt.axis('off')
+
+
+
+
+
+
+plt.show()
+
+
